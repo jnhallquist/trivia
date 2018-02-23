28045d05fd7b53e7bb93fdf3d0fceff18bee45e4
Jennifer Hallquist <jnhallquist@gmail.com>
2018-02-22 20:18:13 -0800

    Add tests for user email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180223033640) do
+ActiveRecord::Schema.define(version: 20180223041539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20180223033640) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
