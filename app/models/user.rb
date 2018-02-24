61f624b84902333b74c1c54c9dfe62da33cd36e5
Jennifer Hallquist <jnhallquist@gmail.com>
2018-02-23 21:29:58 -0800

    Fix more CC errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,12 @@ class User < ApplicationRecord
 
   # Returns the hash digest of the given string.
   def self.digest(string)
-    if ActiveModel::SecurePassword.min_cost
-      cost = BCrypt::Engine::MIN_COST
+    cost = if ActiveModel::SecurePassword.min_cost
+      BCrypt::Engine::MIN_COST
     else
-      cost = BCrypt::Engine.cost
+      BCrypt::Engine.cost
     end
-    
+
     BCrypt::Password.create(string, cost: cost)
   end
 end
