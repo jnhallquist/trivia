3e535addaa1fc108c71fcd863db02ff3c4401989
Jennifer Hallquist <jnhallquist@gmail.com>
2018-02-24 01:02:57 -0800

    Coveralls config changes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,25 +1,4 @@
-require 'simplecov'
 require 'coveralls'
-
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-SimpleCov.start do
-  class LineFilter < SimpleCov::Filter
-    def matches?(source_file)
-      source_file.lines.count < filter_argument
-    end
-  end
-
-  SimpleCov.minimum_coverage 90
-
-  add_group "Models", "app/models"
-  add_group "Controllers", "app/controllers"
-  add_group "Long files" do |src_file|
-    src_file.lines.count > 100
-  end
-  add_group "Multiple Files", ["app/models", "app/controllers"]
-  add_group "Short files", LineFilter.new(5)
-end
-
 Coveralls.wear!('rails')
 
 ENV['RAILS_ENV'] ||= 'test'
