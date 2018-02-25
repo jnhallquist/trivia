40697d273f19d894590e028b45e679cdaf46af23
Jennifer Hallquist <jnhallquist@gmail.com>
2018-02-24 22:07:33 -0800

    Update seed data to give admin privilege to first user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,13 @@
 # Examples:
 #  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #  Character.create(name: 'Luke', movie: movies.first)
-User.create!(email: 'example@fake.org',
+User.create!(email: 'test@test.com',
              password:              'password',
-             password_confirmation: 'password')
+             password_confirmation: 'password',
+             admin: true)
 
 99.times do |n|
-  email = "example-#{n + 1}@fake.org"
+  email = "test-#{n + 1}@test.com"
   password = 'password'
   User.create!(email: email,
                password:              password,
