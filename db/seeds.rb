252155029bfc7a57c2cbd652cf30f36fa197d308
Jennifer Hallquist <jnhallquist@gmail.com>
2018-02-24 21:30:43 -0800

    Seed database with sample users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 # Examples:
 #  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #  Character.create(name: 'Luke', movie: movies.first)
+User.create!(email: 'example@fake.org',
+             password:              'password',
+             password_confirmation: 'password')
+
+99.times do |n|
+  email = "example-#{n + 1}@fake.org"
+  password = 'password'
+  User.create!(email: email,
+               password:              password,
+               password_confirmation: password)
+end
