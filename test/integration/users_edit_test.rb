38f48475a8b0d260bf3a696f90c832d3cf7a4609
Jennifer Hallquist <jnhallquist@gmail.com>
2018-02-24 18:08:46 -0800

    Users edit integration test

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:test)
+  end
+
+  test 'unsuccessful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { email: 'foo@invalid',
+                                              password:              'foo',
+                                              password_confirmation: 'bar' } }
+
+    assert_template 'users/edit'
+  end
+
+  test 'successful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    email = 'foo@bar.com'
+    patch user_path(@user), params: {
+      user: { email: email,
+              password:              'testtest',
+              password_confirmation: 'testtest' }
+    }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal email, @user.email
+  end
+end
